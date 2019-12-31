f9e8258e5b405218579b493eed26a3f56a91073a
Xotl <xotl@Surface-Xotl.localdomain>
2019-12-31 11:51:35 -0600

    Removing hardcoded values

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ const download = require('./download')
 async function main() {
     const githubToken = core.getInput('github_token', { required: true })
     const actionMode = core.getInput('mode', { required: true })
-
-    const owner = 'Xotl'
-    const repo = 'cool-github-releases'
-    // const { name:repo, owner: { name:owner } } = github.context.payload.repository
-    
+    const { name:repo, owner: { name:owner } } = github.context.payload.repository
     const octokit = new github.GitHub(githubToken)
 
     if (actionMode === 'download'){
