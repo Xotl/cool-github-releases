fbaf320d3d21bd32ac374a236c28e69298c4ac99
Xotl <edgardo333@gmail.com>
2019-12-31 15:56:20 -0600

    Removing logs from main (#1)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const core = require('@actions/core');
 
 async function main() {
     const githubToken = core.getInput('github_token', { required: true });
-
-    console.log('It runs!!!!!', githubToken)
     const octokit = new github.GitHub(githubToken);
 
     const releaseResponse = await octokit.repos.getLatestRelease({
