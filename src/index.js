6668e35a51dc45b512281973567abf05aecb83e4
Xotl <xotl@Surface-Xotl.localdomain>
2019-12-31 04:35:09 -0600

    First test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('It runs!!!!!')
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function main() {
+    const githubToken = core.getInput('github_token', { required: true });
+
+    console.log('It runs!!!!!', githubToken)
+    const octokit = new github.GitHub(githubToken);
+
+    const releaseResponse = await octokit.repos.getLatestRelease({
+        owner,
+        repo
+    })
+
+    console.log(releaseResponse);
+}
+
+main()
