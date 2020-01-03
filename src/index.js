c080da5b15ad72743b4aba5fa7bdf91c37a357af
Xotl <xotl@Surface-Xotl.localdomain>
2020-01-03 03:46:24 -0600

    Validate modes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 const github = require('@actions/github')
-const core = require('@actions/core')
+const Core = require('@actions/core')
 
 const updateRelease = require('./update')
 const downloadRelease = require('./download')
 const deleteRelease = require('./delete')
 
-async function main() {
-    const githubToken = core.getInput('github_token', { required: true })
-    const actionMode = core.getInput('mode', { required: true })
+const modeDict = {
+    update: updateRelease,
+    download: downloadRelease,
+    delete: deleteRelease,
+}
 
-    // Ensure it is masked in logs, just in case we happen to log that somewhere
-    core.setSecret(githubToken)
+async function main() {
+    const actionMode = Core.getInput('mode', { required: true })
+    const modeFn = modeDict[actionMode]
+
+    if (!modeFn) {
+        return Core.setFailed(`Unknown mode '${modeFn}', valid values are: ${Object.keys(modeDict).join(', ')}`)
+    }
+
+    const githubToken = Core.getInput('github_token', { required: true })
+    Core.setSecret(githubToken)// Ensure it is masked in logs, just in case we happen to log that
 
     const { name:repo, owner: { name, login } } = github.context.payload.repository
     const octokit = new github.GitHub(githubToken)
     const context = {repo, owner: name || login}
 
-    if (actionMode === 'download') {
-        await downloadRelease(octokit, context, githubToken)
-        return
-    }
-
-    if (actionMode === 'delete') {
-        await deleteRelease(octokit, context)
-        return
-    }
-
-    // Create/Update Release
-    await updateRelease(octokit, context)
+    await modeFn(octokit, context, githubToken)
 }
 
 main()
