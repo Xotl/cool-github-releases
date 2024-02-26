12117cfb70874b91bfbbe44c97765b3bdac77d10
Xotl <7025727+Xotl@users.noreply.github.com>
2024-02-26 21:39:34 +0000

    Fixes bug

--- src/update.js
+++ src/update.js
@@ -137,7 +137,7 @@ module.exports = async (octokit, context) => {
 
     // Create/Update the release values
     const opts = { ...context, body, name, tag_name, draft, prerelease }
-    let releaseObj
+    let releaseObj = null;
     try {
         if (foundRelease) {
             releaseObj = await editRelease(octokit, opts, foundRelease.id)
